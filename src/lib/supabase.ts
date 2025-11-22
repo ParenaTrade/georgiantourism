18657bbbd65d88c1489a8be9b596e89ebffe6ae8
ParenaTrade <parenatrade@gmail.com>
2025-11-22 13:49:03 +0300

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,4 +1,4 @@
-import { createClient } from '@supabase/supabase-js';
+import { supabase } from '@/lib/supabase';
 import { Database } from '@/types/database';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
@@ -38,3 +38,4 @@ export const supabaseAdmin = createClient<Database>(
     },
   }
 );
+
