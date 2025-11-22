c8560b586deda2db8744fdb6e78891bc82877028
ParenaTrade <parenatrade@gmail.com>
2025-11-22 15:04:16 +0300

    Update route.ts

--- src/app/api/auth/callback/route.ts
+++ src/app/api/auth/callback/route.ts
@@ -6,7 +6,7 @@ export async function GET(request: Request) {
   const code = requestUrl.searchParams.get('code');
 
   if (code) {
-       const supabase = createSupabaseClient(); // ✅ Yeni isim
+    const supabase = createSupabaseClient(); // ✅ Yeni isim
     const { error } = await supabase.auth.exchangeCodeForSession(code);
     
     if (error) {
