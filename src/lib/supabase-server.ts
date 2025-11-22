edf7fcdda14eea9d4b9f5040606128861c885641
ParenaTrade <parenatrade@gmail.com>
2025-11-22 14:25:40 +0300

    Update supabase-server.ts

--- src/lib/supabase-server.ts
+++ src/lib/supabase-server.ts
@@ -1,3 +1,4 @@
+// src/lib/supabase-server.ts
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
 import { cookies } from 'next/headers';
 import { Database } from '@/types/database';
